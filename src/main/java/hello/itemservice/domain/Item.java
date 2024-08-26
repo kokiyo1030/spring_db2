d3bbe430738a4be38e87ea36aa8d785a8246281f
kokiyo1030 <kokiyo1030@naver.com>
2024-08-26 15:42:06 +0900

    jpa

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -1,16 +1,22 @@
 package hello.itemservice.domain;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity
+//@Table(name = "item") //객체명과 같으면 생략가능
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
 
+    //JPA는 public 또는 private 기본 생성자가 필수!
     public Item() {
     }
 
